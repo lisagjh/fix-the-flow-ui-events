bb6ad21b918dc65e4861c2f15056f25ce36f8ac4
lisagjh <lisagjhoorn@outlook.com>
2024-12-09 11:13:58 +0100

    drag wip

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,42 +1,52 @@
+let interaction = document.querySelector("a:nth-of-type(12)");
 
-let interaction = document.querySelector('a:nth-of-type(12)')
-
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interaction.addEventListener("click", jumpHandler);
+interaction.addEventListener("animationend", jumpHandler);
 
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+  interaction.classList.toggle("jump");
 }
 
 // ! mijn code
 
 // button 1
-// maakt object van de button 
-const negativeBtn = document.querySelector('a:nth-of-type(1')
+// maakt object van de button
+const negativeBtn = document.querySelector("a:nth-of-type(1");
 // dit zorgt dat er bij een click event de class negative getoggled word. toggle kan worden vervangen door add of remove voor het gewenste effect
-negativeBtn.addEventListener('click', function() {
-  negativeBtn.classList.toggle("negative")
-})
+negativeBtn.addEventListener("click", function () {
+  negativeBtn.classList.toggle("negative");
+});
 
 //  button 2
 
-const jelloBtn = document.querySelector('a:nth-of-type(2)')
+const jelloBtn = document.querySelector("a:nth-of-type(2)");
 
-jelloBtn.addEventListener('mouseover', function() {
-  jelloBtn.classList.add('jelloHor')
-})
+jelloBtn.addEventListener("mouseover", function () {
+  jelloBtn.classList.add("jelloHor");
+});
 
-jelloBtn.addEventListener('mouseout', function() {
-  jelloBtn.classList.remove('jelloHor')
-})
+jelloBtn.addEventListener("mouseout", function () {
+  jelloBtn.classList.remove("jelloHor");
+});
 
 // button 4
-const pulseBtn = document.querySelector('a:nth-of-type(4)')
+const pulseBtn = document.querySelector("a:nth-of-type(4)");
 
-pulseBtn.addEventListener('mouseover', function() {
-  pulseBtn.classList.add('pulse')
-})
+pulseBtn.addEventListener("mouseover", function () {
+  pulseBtn.classList.add("pulse");
+});
 
-pulseBtn.addEventListener('mouseout', function() {
-  pulseBtn.classList.remove('pulse')
-})
+pulseBtn.addEventListener("mouseout", function () {
+  pulseBtn.classList.remove("pulse");
+});
+
+// button 5
+
+let dragBtn = document.querySelector("a:nth-of-type(5)");
+
+dragBtn.ondrag = drag;
+
+function drag(event) {
+  console.log("dragging")
+  event.dataTransfer.setData("text", event.target.id);
+  }
